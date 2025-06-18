95c5defa3de4c95dcbdca0535789385f9dc397f7
RisellU <risellurdaneta@gmail.com>
2025-06-18 12:32:03 -0500

    Uso de react server component, cliente y servidor. uso de archivo proxy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+"use client" // con este anotacion hace que la pagina se convierta en un componente cliente
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  console.log('hola amigos')
   return (
     <div className={styles.page}>
       <main className={styles.main}>
---
+++ src/components/Header/index.ts
@@ -0,0 +1,5 @@
+// Este es un archivo proy que sirve para simplificar las importaciones, es decir, 
+// agrupa las importaciones en un solo lugar y en el archivo principal se importa 
+// todos los componentes directamente del archivo proxy
+
+export { Header } from './Header'
