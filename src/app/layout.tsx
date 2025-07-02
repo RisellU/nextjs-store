ddb5c9d5b9820fc29413e46e8104a38b3bfd47e8
RisellU <risellurdaneta@gmail.com>
2025-07-02 10:12:08 -0500

    componente image y estilos globales

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
+import { Raleway } from 'next/font/google'
 import { Header } from "app/components/shared/Header";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Footer } from 'app/components/shared/Footer'
+import "app/sass/globals.sass";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const raleway = Raleway({
+  subsets: ['latin'],
+  weight: ['100','300','500','700']
+})
 
 export default function RootLayout({
   children,
@@ -19,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={raleway.className}>
         <Header />
         {children}
         <Footer />
